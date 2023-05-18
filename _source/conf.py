781a08713985ec60ba1e16e959248656af3bc8d1
Michael Walter <michael.walter@gmail.com>
2023-05-18 16:23:42 +0200

    Update Makefile and re-generate docs

--- _source/conf.py
+++ _source/conf.py
@@ -1,21 +1,18 @@
 import os, sys
-from recommonmark.parser import CommonMarkParser
 
-extensions = []
-source_parsers = {'.md': CommonMarkParser}
-source_suffix = ['.md']
-project = 'Kronecker'
-master_doc = 'index'
-exclude_patterns = ['_build']
-html_title = ''
-html_theme = 'alabaster'
+extensions = ["recommonmark"]
+project = "Kronecker"
+master_doc = "index"
+exclude_patterns = ["_build"]
+html_title = ""
+html_theme = "alabaster"
 html_theme_options = {
-    'nosidebar': True,
-    'github_user': 'catch22',
-    'github_repo': 'kronecker',
-    'github_banner': True,
-    'show_powered_by': False,
-    'page_width': '940px'
+    "nosidebar": True,
+    "github_user": "qi-rub",
+    "github_repo": "kronecker",
+    "github_banner": True,
+    "show_powered_by": False,
+    "page_width": "940px",
 }
 html_domain_indices = False
 html_use_index = False
